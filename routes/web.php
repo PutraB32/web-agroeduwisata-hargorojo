fca4adc5f855db71077f9540c2bf96b18fde6b91
PutraB32 <putrabhato12@students.amikom.ac.id>
2026-01-22 21:40:38 +0700

    Initial commit web agroeduwisata Hargorojo

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Dashboard;
+use Illuminate\Support\Facades\Route;
+
+// Route::middleware('guest')->group(function () {
+//     Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+//     Route::post('/login', 'Auth\LoginController@login');
+//     Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+//     Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+//     Route::post('/register', 'Auth\RegisterController@register');
+// });
+
+Route::get('/', [Dashboard::class, 'index'])->name('home');
