8a8c84a9c1e2eb80c84c984568dd7b3a5e2873d1
PutraB32 <putrabhato12@students.amikom.ac.id>
2026-04-21 18:42:19 +0700

    setup routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Http\Controllers\Dashboard;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\ProfilDesaController;
+use App\Http\Controllers\AgroeduwisataController;
+use App\Http\Controllers\ProdukGulaKelapaController;
+use App\Http\Controllers\EcommerceController;
+use App\Http\Controllers\KatalogDesaController;
+use App\Http\Controllers\KontakController;
 
 // Route::middleware('guest')->group(function () {
 //     Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -12,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 //     Route::post('/register', 'Auth\RegisterController@register');
 // });
 
-Route::get('/', [Dashboard::class, 'index'])->name('home');
+Route::get('/', [BerandaController::class, 'index'])->name('home');
+Route::get('/beranda', [BerandaController::class, 'index'])->name('beranda');
+Route::get('/profil', [ProfilDesaController::class, 'index'])->name('profil');
+Route::get('/agroeduwisata', [AgroeduwisataController::class, 'index'])->name('agro');
+Route::get('/produk', [ProdukGulaKelapaController::class, 'index'])->name('produk');
+Route::get('/ecommerce', [EcommerceController::class, 'index'])->name('ecommerce');
+Route::get('/katalog', [KatalogDesaController::class, 'index'])->name('katalog');
+Route::get('/kontak', [KontakController::class, 'index'])->name('kontak');
