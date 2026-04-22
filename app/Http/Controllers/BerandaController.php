ed9739e4ddd01d3e99f76d9c55e0209aa73c67cf
PutraB32 <putrabhato12@students.amikom.ac.id>
2026-04-22 22:17:06 +0700

    edit beranda conttroller

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Testimoni;
+use App\Models\Agroeduwisata;
+use App\Models\Produk;
 
 class BerandaController extends Controller  
 {
