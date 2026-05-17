f9ff395961f62174e84f95dda62c0302a6816242
Dimas Andhika Triwardana <dimasandhikatriwardana@students.amikom.ac.id>
2026-05-17 23:50:10 +0700

    update resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
 import './bootstrap';
+
+
+window.counter = function(target, speed = 40) {
+    return {
+        count: 0,
+        target: target,
+        started: false,
+        init() {
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting && !this.started) {
+                        this.started = true
+                        let interval = setInterval(() => {
+                            if (this.count < this.target) {
+                                this.count++
+                            } else {
+                                clearInterval(interval)
+                            }
+                        }, speed)
+                    }
+                })
+            }, {
+                threshold: 0.5
+            })
+            observer.observe(this.$el)
+        }
+    }
+}
