aed7973e422f4e218d1ff0fad515e0b034408b79
PutraB32 <putrabhato12@students.amikom.ac.id>
2026-06-16 02:18:17 +0700

    update controller katalog desa

--- app/Http/Controllers/KatalogDesaController.php
+++ app/Http/Controllers/KatalogDesaController.php
@@ -2,26 +2,67 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\KatalogDesa;
+use Illuminate\Http\Request;
 
 class KatalogDesaController extends Controller
 {
     public function index(Request $request)
     {
-        $semuaKatalog = KatalogDesa::with('kategoriKatalog')->latest()->get()->groupBy(function($item) {
-            return $item->kategoriKatalog->nama_kategori ?? 'Lainnya';
-        });
+        $pengumuman = KatalogDesa::with('kategoriKatalog')
+            ->whereHas('kategoriKatalog', function ($query) {
+                $query->where('nama_kategori', 'Pengumuman');
+            })
+            ->latest()
+            ->take(4)
+            ->get();
 
-        $pengumuman = $semuaKatalog->get('Pengumuman', collect());
-        $artikelBerita = $semuaKatalog->get('Artikel', collect());
-        $perpustakaan = $semuaKatalog->get('Perpustakaan', collect());
-        $galeri = $semuaKatalog->get('Galeri', collect());
+        $artikelBerita = KatalogDesa::with('kategoriKatalog')
+            ->whereHas('kategoriKatalog', function ($query) {
+                $query->where('nama_kategori', 'Artikel & Berita');
+            })
+            ->latest()
+            ->take(6)
+            ->get();
+
+        $perpustakaan = KatalogDesa::with('kategoriKatalog')
+            ->whereHas('kategoriKatalog', function ($query) {
+                $query->where('nama_kategori', 'Perpustakaan');
+            })
+            ->latest()
+            ->take(6)
+            ->get();
+
+        $galeri = KatalogDesa::with('kategoriKatalog')
+            ->whereHas('kategoriKatalog', function ($query) {
+                $query->where('nama_kategori', 'Galeri');
+            })
+            ->latest()
+            ->take(8)
+            ->get();
+
+        $beritaUtama = KatalogDesa::with('kategoriKatalog')
+    ->whereHas('kategoriKatalog', function ($query) {
+        $query->where('nama_kategori', 'Artikel & Berita');
+    })
+    ->latest()
+    ->first();
+
+        $sidebarBerita = KatalogDesa::with('kategoriKatalog')
+            ->whereHas('kategoriKatalog', function ($query) {
+                $query->where('nama_kategori', 'Artikel & Berita');
+            })
+            ->latest()
+            ->skip(1)
+            ->take(3)
+            ->get();
 
         return view('pages.katalogdesa', compact(
-            'pengumuman', 
-            'artikelBerita', 
-            'perpustakaan', 
+            'beritaUtama',
+            'sidebarBerita',
+            'pengumuman',
+            'artikelBerita',
+            'perpustakaan',
             'galeri'
         ));
     }
