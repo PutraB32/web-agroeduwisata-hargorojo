21bc11c10fc83fefe062d02e66d76c9bd278c98c
PutraB32 <putrabhato12@students.amikom.ac.id>
2026-07-06 03:00:40 +0700

    update code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->validateCsrfTokens(except: [
             'payment/midtrans/notification',
+            'payment/midtrans/notification*',
         ]);
 
         $middleware->alias([
