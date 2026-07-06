b54182bfa3cebc8c7e5c5f3dac8af856e438514a
Dimas Andhika Triwardana <dimasandhikatriwardana@students.amikom.ac.id>
2026-07-06 18:51:41 +0700

    update code

--- resources/js/order-notification.js
+++ resources/js/order-notification.js
@@ -70,6 +70,7 @@ function initOrderNotification() {
         trigger.addEventListener("click", () => {
             const newSeenUpdates = { ...getSeenUpdates(storageKey), ...currentUpdates };
             setSeenUpdates(storageKey, newSeenUpdates);
+            newOrderId = 0;
             setBadgeCount(badges, 0);
         });
     });
